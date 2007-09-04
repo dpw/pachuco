d55f247cb07f2582e565a38239ca6a2118532065
David Wragg <david@wragg.org>
2007-09-04 20:30:39 +0100

    Fix the timing code to work under the interpreter

--- test.lisp
+++ test.lisp
@@ -127,25 +127,24 @@
                  '((1 2) (3 4) 5 6 7 8)))
 
 (when-compiling
-  (defmacro (time form)
-    (quasiquote (begin
-      (define count 0)
-      (define min 0)
-      (while (< count 100)
-        (define start-t (raw-rdtsc))
-        (unquote form)
-        (define end-t (raw-rdtsc))
-        (define delta (- (car end-t) (car start-t)))
-        (set! min (if (or (= 0 min) (< delta min)) delta min))
-        (set! count (1+ count)))
+  (define (time-function f)
+    (define count 0)
+    (define min 0)
+    (while (< count 100)
+      (define start-t (raw-rdtsc))
+      (f)
+      (define end-t (raw-rdtsc))
+      (define delta (- (car end-t) (car start-t)))
+      (set! min (if (or (= 0 min) (< delta min)) delta min))
+      (set! count (1+ count)))
 
-      (formout stdout "Cycles: ~S~%" min)))))
+    (formout stdout "Cycles: ~S~%" min)))
 
-;(when-interpreting
-;  (defmacro (time form) form))
+(when-interpreting
+  (define (time-function f) (f)))
 
 (define (main)
-  (time (tests)))
+  (time-function tests))
 
 (define (foo)
   (define dual-env (cons (make-initial-macro-env) (make-initial-interpreter-env)))
