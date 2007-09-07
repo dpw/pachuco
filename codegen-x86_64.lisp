f59eea62499a29f8caa8825855ca80f558d75aaf
David Wragg <david@wragg.org>
2007-09-07 01:34:53 +0100

    Move c-call stuff to codegen-x86_64
    The register args and stack alignment differences mean that the x86_64
    and i386 c-calls are not so similar.

--- codegen-x86_64.lisp
+++ codegen-x86_64.lisp
@@ -39,3 +39,34 @@
   (emit-set-ac-flag out false)
   (dolist (reg (reverse c-callee-saved-regs)) (emit-pop out reg))
   (emit out "ret"))
+
+;;; C calls
+
+(define-reg-use (c-call c-function-name . args)
+  (rplaca (cdr form) (list (cons 'c-function-name c-function-name)))
+  (operator-args-reg-use form)
+  general-register-count)
+
+(define (c-callee-saved reg) (member? reg c-callee-saved-regs))
+
+(define-codegen (c-call attrs . args)
+  (when (> (length args) (length c-call-arg-regs))
+    (error "too many arguments to c-call"))
+  (operator-args-codegen form in-frame-base
+                      (move-regs-to-front c-call-arg-regs general-registers)
+                      out)
+  (unless (c-callee-saved %alloc) (emit-push out %alloc))
+  (emit out "cld")
+  (emit-set-ac-flag out false)
+
+  ;; C ABI requires us to align stack to 16 byte bundary
+  (emit-push out %bp)
+  (emit-mov out %sp %bp)
+  (emit-and out (immediate -16) %sp)
+  (emit out "call ~A" (attr-ref attrs 'c-function-name))
+  (emit out "leave")
+
+  (emit-set-ac-flag out true)
+  (unless (c-callee-saved %alloc) (emit-pop out %alloc))
+  (unless (c-callee-saved %func) (emit-restore-%func out))
+  (emit-convert-value out %a dest in-frame-base out-frame-base))
