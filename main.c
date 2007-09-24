4f385825aaa7407383539ad649d448ac58939457
David Wragg <david@wragg.org>
2007-09-24 01:04:31 +0100

    mmap the heap

--- main.c
+++ main.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
+#include <sys/mman.h>
 
-extern long lisp(long heap);
-
-long heap[1000000];
+extern long lisp(long heap_end);
 
 int main(void)
 {
-        long alloc_before = (long)heap + sizeof(heap);
-        long alloc_after = lisp(alloc_before);
+	long heap_size = 128 * 1024 * 1024; /* 128MB */
+	void *heap = mmap(NULL, heap_size, PROT_READ|PROT_WRITE,
+			  MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+	long alloc_before, alloc_after;
 
-        fprintf(stderr, "[%ld bytes allocated]\n", alloc_before - alloc_after);
-        return 0;
+	if (heap == MAP_FAILED)
+		perror("mmap heap");
+
+	alloc_before = (long)heap + heap_size;
+	alloc_after = lisp(alloc_before);
+
+	fprintf(stderr, "[%ld bytes allocated]\n", alloc_before - alloc_after);
+	return 0;
 }
