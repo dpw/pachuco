cdff09880b7d54374751d6f02d3ec3fc75077b63
David Wragg <david@wragg.org>
2008-03-25 22:02:34 +0000

    Convert from rdtsc to gettimeofday.  Make output more regular in benchmarking mode.

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <inttypes.h>
 #include <sys/mman.h>
+#include <sys/time.h>
 
 #define HEAP_SIZE (512UL * 1024 * 1024) /* 512MB */
 
@@ -10,48 +10,43 @@ unsigned long heap_alloc;
 extern void lisp();
 char **lisp_argv;
 
-inline uint64_t rdtsc()
+static void run_lisp(void *heap, unsigned long *alloced, double *time_taken)
 {
-#if defined(__i386__)
-        uint64_t val;
-        __asm__ __volatile__ ("rdtsc" : "=A" (val));
-        return val;
-#elif defined(__x86_64__)
-        unsigned int a,d;
-        asm volatile("rdtsc" : "=a" (a), "=d" (d));
-        return ((unsigned long)a) | (((unsigned long)d)<<32);
-#else
-#error "Unsupported architecture"
-#endif
-}
-
-void run_lisp(void *heap)
-{
+        struct timeval t1, t2;
         unsigned long heap_end = (unsigned long)heap + HEAP_SIZE;
+
         heap_alloc = heap_end;
-        uint64_t start = rdtsc();
+        gettimeofday(&t1, NULL);
         lisp();
-        uint64_t end = rdtsc();
-        fprintf(stderr, "[%ld bytes allocated ; %" PRIu64 " cycles]\n",
-                heap_end - heap_alloc, end - start);
+        gettimeofday(&t2, NULL);
+        *alloced = heap_end - heap_alloc;
+        *time_taken
+                = (t2.tv_sec - t1.tv_sec) + (t2.tv_usec - t1.tv_usec) * 1.0e-6;
 }
 
 int main(int argc, char **argv)
 {
-        lisp_argv = argv;
+        unsigned long alloced;
+        double time_taken;
         void *heap = mmap(NULL, HEAP_SIZE, PROT_READ|PROT_WRITE,
                           MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
         if (heap == MAP_FAILED)
                 perror("mmap heap");
 
+        lisp_argv = argv;
+
         char *iterations = getenv("BENCHMARK_ITERATIONS");
         if (iterations == NULL) {
-                run_lisp(heap);
+                run_lisp(heap, &alloced, &time_taken);
+                fprintf(stderr, "(%lu bytes allocated; %g seconds)\n",
+                        alloced, time_taken);
         }
         else {
                 int i;
-                for (i = atoi(iterations); i > 0; i--)
-                        run_lisp(heap);
+                for (i = atoi(iterations); i > 0; i--) {
+                        run_lisp(heap, &alloced, &time_taken);
+                        fprintf(stderr, "%g\n", time_taken);
+                }
         }
 
         return 0;
