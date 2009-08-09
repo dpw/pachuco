a846692b0686f9adb716bc900f393ba5695d6723
David Wragg <david@wragg.org>
2009-08-09 18:58:17 +0100

    munmap unused semispace between GCs, and various cleanups
    Previously we would allocate both semispaces at startup, and then
    alternate between them.  This works well if you have lots of physical
    memory available, but not so well if the total size of the two semispaces
    approaches physical memory size: The unused one could start to get swapped
    out, resulting in painful GC pauses.
    
    So instead, just have one semispace allocated most of the time, and
    only allocate a second during the GC process, retiring from-space at
    the end of GC.
    
    Also, various cleanups in the area.  In particular, BENCHMARK_ITERATIONS
    is gone, I haven't used it in a long time.

--- runtime/main.c
+++ runtime/main.c
@@ -4,41 +4,40 @@
 #include <sys/mman.h>
 #include <sys/time.h>
 
-/* Heap GC threshold size, */
-unsigned long heap_threshold_size;
+/* Size of a heap semispace */
+unsigned long heap_size = 32UL * 1024 * 1024;
 
-unsigned long heap_start;
-unsigned long heap_end;
-unsigned long heap_alloc;
-unsigned long heap_threshold;
-
-unsigned long heap2_start;
-unsigned long heap2_end;
+unsigned long heap_start, heap_end, heap_alloc, heap_threshold;
 
 char *verbose_gc;
 
 extern void lisp();
 char **lisp_argv;
 
-static unsigned long alloc_heap(unsigned long size)
+unsigned long alloc_heap()
 {
-        void *heap = mmap(NULL, size, PROT_READ|PROT_WRITE,
+        void *heap = mmap(NULL, heap_size, PROT_READ|PROT_WRITE,
                           MAP_PRIVATE|MAP_ANON, -1, 0);
-        if (heap == MAP_FAILED)
+        if (heap == MAP_FAILED) {
                 perror("mmap heap");
+                abort();
+        }
 
         return (unsigned long)heap;
+}
+
+void free_heap(unsigned long addr)
+{
+	if (munmap((void *)addr, heap_size)) {
+		perror("munmap heap");
+		abort();
+	}
 }
 
 int main(int argc, char **argv)
 {
         /* HEAP_SIZE specifies the size of the heap semi-spaces, in MB. */
         char *heap_size_env = getenv("HEAP_SIZE");
-        unsigned long heap_size = 32UL * 1024 * 1024;
-
-        /* BENCHMARK_ITERATIONS specifies how many iterations of the
-        program to run, for benchmarking purposes. */
-        char *iterations = getenv("BENCHMARK_ITERATIONS");
 
         /* Setting VERBOSE_GC displays GC progress on stderr. */
         verbose_gc = getenv("VERBOSE_GC");
@@ -46,39 +45,12 @@ int main(int argc, char **argv)
         if (heap_size_env)
                 heap_size = (unsigned long)atoi(heap_size_env) * 1024 * 1024;
         
-        /* Looking at this months later, it seems bogus.  I think the
-           idea was to leave a bit of space in the heap for the GC to
-           use up to the point when it switches allocations over to
-           to-space.  But of course, any allocations before that will
-           be subject to the old heap_threshold, and cause a recursive
-           GC.  So evidently it is unncecessary.  For this to really
-           work, the first thing the GC should do is to adjust
-           heap_threshold to allow itself access to the slack. */
-        heap_threshold_size = heap_size - 4 * 1024;
-        
-        heap_start = alloc_heap(heap_size);
-        heap_end = heap_start + heap_size;
-
-        heap2_start = alloc_heap(heap_size);
-        heap2_end = heap2_start + heap_size;
+        heap_threshold = heap_start = alloc_heap(heap_size);
+        heap_alloc = heap_end = heap_start + heap_size;
 
         lisp_argv = argv;
 
-        if (iterations == NULL) {
-                heap_alloc = heap_end;
-                heap_threshold = heap_end - heap_threshold_size;
-                lisp();
-        }
-        else {
-                int i;
-                for (i = atoi(iterations); i > 0; i--) {
-                        heap_alloc = heap_end;
-                        heap_threshold = heap_end - heap_threshold_size;
-                        memset((void *)heap_start, 0, heap_end - heap_start);
-                        memset((void *)heap2_start, 0, heap2_end - heap2_start);
-                        lisp();
-                }
-        }
-
+	lisp();
+	
         return 0;
 }
